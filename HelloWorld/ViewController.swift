fc550117962557012da77078514f03b032212db8
Robert Lazarony <robert_lazarony@abercrombie.com>
2016-11-13 15:06:10 -0500

    initial commit

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lowerBoundFeild: UITextField!
+    @IBOutlet var upperBoundFeild: UITextField!
+    @IBOutlet var result: UILabel!
+    
+    @IBAction func generateNumber () {
+        //do some stuff to generate a random number
+        let upper: UInt32 = UInt32(upperBoundFeild.text!)!
+        let lower: UInt32 = UInt32(lowerBoundFeild.text!)!
+        let rand: String? = String(arc4random_uniform(upper - lower) + lower)
+
+        result.text = "Result: " + rand!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        lowerBoundFeild.text = "10"
+        upperBoundFeild.text = "64"
     }
 
     override func didReceiveMemoryWarning() {
